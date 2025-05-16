c9f34c640ce3f1fedb6c012b715b33c96cb3fb2f
BrianLacey <ss3teddy@gmail.com>
2025-05-15 21:35:00 -0700

    Selection UI feedback working correctly with Redux as source of truth.

--- Client/src/components/RenderQuestion.tsx
+++ Client/src/components/RenderQuestion.tsx
@@ -1,37 +1,29 @@
-import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Button } from "@headlessui/react";
 import { IQuiz, ISelected, IState } from "../models";
-import { addSelected } from "../slices/selectedSlice";
+import { updateSelected } from "../slices/selectedSlice";
 
 const RenderQuestion = ({ quiz }: { quiz: IQuiz }) => {
   const dispatch = useDispatch();
   const { question, allAnswers } = quiz;
-  const [select, setSelect] = useState("");
-  const selected: ISelected[] = useSelector((state: IState) => state.quiz);
+  const [selected]: ISelected[] = useSelector((state: IState) => state.selected).filter((item) => item.question === question);
 
   const handleSelected = (e) => {
     const { textContent } = e.target;
-    setSelect(textContent);
-    dispatch(addSelected({ question, answer: textContent }));
+    dispatch(updateSelected({ question, answer: textContent }));
   };
 
   return (
     <div className="flex flex-col pl-8 gap-y-8 text-3xl">
-      {question.replace(/&#039;/g, "'").replace(/&quot;/g, '"')}
+      {question.replace(/&#039;/g, "\'").replace(/&quot;/g, '\"').replace(/&amp;/g, "\&")}
       <div className="flex flex-row gap-x-8">
         {allAnswers.map((answer) => {
           return (
             <Button
               key={answer}
               className={`${
-                // Figure out how to distinguish WHICH answer is getting compared here
-                // Maybe filter method on selected declaration?
-                // Need to change addSelected method to updateSelected.
-                // Maybe change data structure of selected in store from array to object. 
-                selected.answer === answer ? "bg-yellow-950 text-white" : ""
-                // select === answer ? "bg-yellow-950 text-white" : ""
-              } border-yellow-950 border-2 text-lg font-medium p-4.5 rounded-xl hover:border-slate-900 hover:bg-yellow-950 hover:text-white`}
+                selected?.answer === answer ? "bg-yellow-950 text-white" : ""
+                } border-yellow-950 border-2 text-lg font-medium p-4.5 rounded-xl hover:border-slate-900 hover:bg-yellow-950 hover:text-white`}
               onClick={handleSelected}
             >
               {answer}
--- Client/src/slices/selectedSlice.ts
+++ Client/src/slices/selectedSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 import { ISelected } from "../models";
 
 const initialState: ISelected[] = [];
@@ -8,9 +8,27 @@ const selectedSlice = createSlice({
   initialState,
   reducers: {
     addSelected: (state, action) => [...action.payload],
+    updateSelected: (state, action) => {
+      const { payload } = action;
+      const curState = current(state);
+      const nextIndex = curState.findIndex((item) =>
+        item.question === payload.question
+      );
+      if (nextIndex === -1) {
+        return curState.concat([payload]);
+      } else {
+        return curState.map((item, index) => {
+          if (index === nextIndex) {
+            return payload;
+          } else {
+            return item;
+          }
+        })
+      }
+    },
     removeSelected: (state, action) => {},
   },
 });
 
-export const { addSelected, removeSelected } = selectedSlice.actions;
+export const { addSelected, updateSelected, removeSelected } = selectedSlice.actions;
 export default selectedSlice.reducer;
